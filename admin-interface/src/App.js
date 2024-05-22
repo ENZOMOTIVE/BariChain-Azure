cdcd4ca7075483be243a26737d493e97c5f71289
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-22 16:20:23 +0530

    Updated admininterface

--- admin-interface/src/App.js
+++ admin-interface/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './App.css';
 function AdminInterface() {
   const [adminCriteria, setAdminCriteria] = useState({ ph: '', temp: '', turbidity: '' });
 
